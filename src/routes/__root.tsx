ee15a8a1d101dff12e99cd3197d5f8b34a7cb683
tanishq20 <tanishqkumarsgr98@gmail.com>
2026-06-24 20:26:17 +0530

    font update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -106,12 +106,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       },
     ],
     links: [
-      { rel: "preconnect", href: "https://fonts.googleapis.com" },
-      { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,400&family=Caveat:wght@500;700&display=swap",
-      },
     ],
   }),
   component: RootComponent,
